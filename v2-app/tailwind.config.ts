8ff73e36cfb8ef3af7ce5bcfab88cb99dd80ad06
nextgen-web-studio <fgh782893@gmail.com>
2026-09-16 12:13:22 +0530

    Fix tailwind config TS format

--- v2-app/tailwind.config.ts
+++ v2-app/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -20,4 +21,5 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
+export default config
