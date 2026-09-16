02fc6e87cecfec75eaf0e4474b41dd2a4a76dc92
nextgen-web-studio <fgh782893@gmail.com>
2026-09-16 11:06:08 +0530

    Force update file for Vanilla Vite

--- v2-app/vite.config.js
+++ v2-app/vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        booking: resolve(__dirname, 'booking.html'),
+        menu: resolve(__dirname, 'menu.html'),
+        corporate: resolve(__dirname, 'corporate.html'),
+        membership: resolve(__dirname, 'membership.html'),
+        party: resolve(__dirname, 'party.html')
+      }
+    }
+  }
+});
