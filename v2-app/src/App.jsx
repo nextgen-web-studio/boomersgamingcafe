bba5bd2afbc8e620e7ed520efffae84f4c0cea10
nextgen-web-studio <fgh782893@gmail.com>
2026-09-16 11:27:38 +0530

    Add all options to App.jsx

--- v2-app/src/App.jsx
+++ v2-app/src/App.jsx
@@ -4,6 +4,10 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Tournaments from './components/Tournaments';
 import BookingWizard from './components/BookingWizard';
+import Menu from './components/Menu';
+import Membership from './components/Membership';
+import Corporate from './components/Corporate';
+import Admin from './components/Admin';
 
 function Home() {
   return (
@@ -14,6 +18,15 @@ function Home() {
       </section>
       <section id="tournaments" className="py-20 px-4 md:px-8 bg-black/50">
         <Tournaments />
+      </section>
+      <section id="menu" className="py-20 px-4 md:px-8">
+        <Menu />
+      </section>
+      <section id="membership" className="py-20 px-4 md:px-8 bg-black/50">
+        <Membership />
+      </section>
+      <section id="corporate" className="py-20 px-4 md:px-8">
+        <Corporate />
       </section>
     </main>
   );
@@ -26,6 +39,7 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </div>
     </BrowserRouter>
