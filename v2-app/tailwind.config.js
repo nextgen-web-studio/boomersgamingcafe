6e388106ce5f2aa577bc995bc3bc3f92f09a33f7
nextgen-web-studio <fgh782893@gmail.com>
2026-09-16 10:19:10 +0530

    Init V2 React Component: v2-app/tailwind.config.js

---
+++ v2-app/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgc: {
+          dark: '#08090c',
+          yellow: '#efbd4e',
+          lime: '#00db78',
+          card: '#15161a',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
